fdc53fec9105fbff3e34312fa0ab266b05bbe883
a-hart13 <42987457+a-hart13@users.noreply.github.com>
2019-12-15 17:31:03 -0500

    Add files via upload

--- js/levels.js
+++ js/levels.js
@@ -1,17 +1,16 @@
 //This file keeps track of the hero properties, like texture, speed etc.
 //It also keeps track of the ball population rate and fall rate.
 //It also keeps track of the children population rate and running path complexity.
-//IF YOU WANT TO ADD MORE LEVELS, CHANGE MAXLEVEL CONSTANT IN HTML FILE.
+
 var level1 = {
     //ball info
-    numBalls: 1,
+    numBalls: 10,
     rateOfFall: .4,
     populationRate: 50,
 
     //child info
     childSpeed : .15,
-    pathLength: 70,
-    pathComplexity: 2,
+    pathLength: 150,
     numKids: 2,
 
     //hero info
@@ -20,13 +19,13 @@ var level1 = {
 
 var level2 = {
     //ball info
-    numBalls: 1,
+    numBalls: 15,
     rateOfFall: .4,
     populationRate: 50,
 
     //child info
-    childSpeed : .15,
-    pathLength: 70,
+    childSpeed : .20,
+    pathLength: 150,
     pathComplexity: 4,
     numKids: 4,
 }
@@ -38,8 +37,8 @@ var level3 = {
     populationRate: 50,
 
     //child info
-    childSpeed : .15,
-    pathLength: 70,
+    childSpeed : .25,
+    pathLength: 150,
     pathComplexity: 6,
-    numKids: 6,
+    numKids: 8,
 }
